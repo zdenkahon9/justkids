f6037f1ecae198ddacf741f69973957f27d08ad1
zdenka honzalova <zpalffyova@gmail.com>
2026-05-12 19:29:09 +0200

    changes in locations.astro for mobile

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -14,13 +14,13 @@ export type Location = {
 export const locations: Location[] = [
   {
     id: "zdice",
-    name: "Fitcentrum Zdice",
+    name: "Zdice",
     street: "Husova 964",
     city: "Zdice",
     zip: "267 51",
     parking: "Parkování je možné na náměstí nad prodejnou Coop.",
     mapUrl:
-      "https://www.google.com/maps/search/?api=1&query=Fitcentrum+Zdice+Husova+964",
+      "https://www.google.com/maps/search/?api=1&query=Husova+964%2C+267+51+Zdice",
     mapCoords: { lat: 49.912799, lng: 13.97766 },
     accent: "sky",
   },
